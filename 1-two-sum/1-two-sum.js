5cc206e147227773afdc14f582770a9567c0440a
Ankur <44092698+AnkurNahar@users.noreply.github.com>
2022-06-01 16:10:49 +0600

    Time: 117 ms (48.10%), Space: 42.5 MB (68.93%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,16 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    if(nums.length > 2){
-         for(let i=0; i<nums.length; i++){
-             for(let j=i+1; j<nums.length;j++){
-                if(nums[i]+nums[j]===target){
-                    return [i,j]
-                }
-             }
+     for(let i=0; i<nums.length; i++){
+         for(let j=i+1; j<nums.length;j++){
+            if(nums[i]+nums[j]===target){
+                return [i,j]
+            }
          }
-    }
-
-    return [0, 1];
+     }
+     return [];
 
 };
