e544d7b32e8033feb467eda65dcbe942ea278f6e
Ankur <44092698+AnkurNahar@users.noreply.github.com>
2022-06-01 19:17:58 +0600

    Time: 117 ms (5.50%), Space: 42 MB (78.90%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isThree = function(n) {
+    let divisor, count = 2;
+    for(divisor = 2; divisor < n; divisor++){
+        if(divisor != n && n % divisor === 0){
+            count++;
+        }
+    }
+    
+    if (count === 3){
+        return true;
+    }
+    return false;
+};
